cbc16210b50ef9923a86aefab94348874148871a
ilyasbabu2400030895 <2400030895@kluniversity.in>
2026-03-05 22:13:18 +0530

    first commit

--- src/main/java/com/example/skill2/Product.java
+++ src/main/java/com/example/skill2/Product.java
@@ -12,26 +12,44 @@ public class Product {
 
     private String name;
     private String description;
-    private int price;
+    private double price;
     private int quantity;
 
-    public Product() {}
+    public Product() {
+    }
 
-    public Product(String name, String description, int price, int quantity) {
+    public Product(String name, String description, double price, int quantity) {
         this.name = name;
         this.description = description;
         this.price = price;
         this.quantity = quantity;
     }
 
-    public int getProductId() { return productId; }
-    public String getName() { return name; }
-    public String getDescription() { return description; }
-    public int getPrice() { return price; }
-    public int getQuantity() { return quantity; }
+    public int getProductId() {
+        return productId;
+    }
 
-    public void setName(String name) { this.name = name; }
-    public void setDescription(String description) { this.description = description; }
-    public void setPrice(int price) { this.price = price; }
-    public void setQuantity(int quantity) { this.quantity = quantity; }
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
